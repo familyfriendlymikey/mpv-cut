613103c82ee6bfee4b8a67fbe6fe8e298fa96467
Mikey Oz <ozmichaelt@icloud.com>
2022-02-09 13:43:10 -0800

    remove most toggles including hardsubs. disallow toggling of GENERATE_LIST, users must do that through editing the script.

--- cut.lua
+++ cut.lua
@@ -5,17 +5,12 @@ local GLOBAL_DIR = "~/Desktop"
 local ACTION = "copy"
 local GENERATE_LIST = true
 local USE_GLOBAL_DIR = false
-local OUTPUT_MP4 = false
-local HARDSUBS = false
 
 local ENCODE_CRF = 16
 local ENCODE_PRESET = "superfast"
 
 local KEY_CUT = "c"
 local KEY_TOGGLE_ACTION = "a"
-local KEY_TOGGLE_GENERATE_LIST = "l"
-local KEY_TOGGLE_HARDSUBS = "h"
-local KEY_TOGGLE_MP4 = "m"
 local KEY_TOGGLE_USE_GLOBAL_DIR = "g"
 
 -- END USER CONFIGURATION
@@ -36,10 +31,6 @@ local function cut(start_time, end_time)
 	local ext = mp.get_property("filename"):match("^.+(%..+)$") or ".mp4"
 	local output_dir = mp.command_native({"expand-path", GLOBAL_DIR})
 
-	if OUTPUT_MP4 then
-		ext = ".mp4"
-	end
-
 	if not USE_GLOBAL_DIR then
 		output_dir = utils.join_path(input_dir, "CUTS")
 	end
@@ -47,7 +38,7 @@ local function cut(start_time, end_time)
 	local prefix = ACTION == "encode" and "ENCODE_" or "COPY_"
 	local output_filename = prefix .. filename_noext .. "_FROM_" .. start_time .. "_TO_" .. end_time .. ext
 	local cut_output_path = utils.join_path(output_dir, output_filename)
-	local list_output_path = utils.join_path(output_dir, filename_noext .. ".txt")
+	local list_output_path = utils.join_path(input_dir, "LIST_" .. filename_noext .. ".txt")
 
 	mp.msg.info("ACTION: " .. ACTION)
 	mp.msg.info("INPUT PATH: " .. input_path)
@@ -71,46 +62,41 @@ local function cut(start_time, end_time)
 			cut_output_path
 		)
 	elseif ACTION == "encode" then
-		if HARDSUBS then
-			mp.commandv(
-				"run",
-				"ffmpeg", "-nostdin", "-y",
-				"-ss", start_time,
-				"-copyts",
-				"-i", input_path,
-				"-ss", start_time,
-				"-vf", "subtitles=\'" .. input_path .. "\'",
-				"-t", end_time - start_time,
-				"-pix_fmt", "yuv420p",
-				"-crf", ENCODE_CRF,
-				"-preset", ENCODE_PRESET,
-				cut_output_path
-			)
-		else
-			mp.commandv(
-				"run",
-				"ffmpeg", "-nostdin", "-y",
-				"-ss", start_time,
-				"-i", input_path,
-				"-t", end_time - start_time,
-				"-pix_fmt", "yuv420p",
-				"-crf", ENCODE_CRF,
-				"-preset", ENCODE_PRESET,
-				cut_output_path
-			)
-		end
+		mp.commandv(
+			"run",
+			"ffmpeg", "-nostdin", "-y",
+			"-ss", start_time,
+			"-i", input_path,
+			"-t", end_time - start_time,
+			"-pix_fmt", "yuv420p",
+			"-crf", ENCODE_CRF,
+			"-preset", ENCODE_PRESET,
+			cut_output_path
+		)
 	end
+
+	local before = 0
+	local after = 0
 
 	if ACTION == "list" or GENERATE_LIST then
 		local out_string = "\n" .. mp.get_property("filename") .. ": " .. start_time .. " " .. end_time
 		local file = io.open(list_output_path, "a")
+
+		mp.msg.info(list_output_path)
+		mp.msg.info(file)
+
+		before = file:seek("end")
 		file:write(out_string)
+		after = file:seek("end")
+
+		mp.msg.info(before .. " -> " .. after)
+
 		io.close(file)
 	end
 
 	text_overlay.hidden = true
 	text_overlay:update()
-	mp.osd_message(cut_output_path, 0.5)
+	mp.osd_message("Δ" .. after - before .. ", " .. cut_output_path, 2)
 
 end
 
@@ -119,17 +105,6 @@ local function refresh_osd()
 		tostring(start_time)
 		.. "\nACTION <" .. KEY_TOGGLE_ACTION .. ">: " .. ACTION
 		.. "\nUSE GLOBAL DIR <" .. KEY_TOGGLE_USE_GLOBAL_DIR .. ">: " .. tostring(USE_GLOBAL_DIR)
-
-	if ACTION == "copy" or ACTION == "encode" then
-		text_overlay.data = text_overlay.data
-			.. "\nGENERATE LIST <" .. KEY_TOGGLE_GENERATE_LIST .. ">: " .. tostring(GENERATE_LIST)
-			.. "\nOUTPUT MP4 <" .. KEY_TOGGLE_MP4 .. ">: " .. tostring(OUTPUT_MP4)
-	end
-
-	if ACTION == "encode" then
-		text_overlay.data = text_overlay.data
-			.. "\nHARSUBS <" .. KEY_TOGGLE_HARDSUBS .. ">: " .. tostring(HARDSUBS)
-	end
 
 	text_overlay.hidden = false
 	text_overlay:update()
@@ -156,21 +131,6 @@ local function put_time()
 
 end
 
-local function toggle_hardsubs()
-	HARDSUBS = not HARDSUBS
-	refresh_osd()
-end
-
-local function toggle_mp4()
-	OUTPUT_MP4 = not OUTPUT_MP4
-	refresh_osd()
-end
-
-local function toggle_generate_list()
-	GENERATE_LIST = not GENERATE_LIST
-	refresh_osd()
-end
-
 local function toggle_use_global_dir()
 	USE_GLOBAL_DIR = not USE_GLOBAL_DIR
 	refresh_osd()
@@ -188,8 +148,5 @@ local function toggle_action()
 end
 
 mp.add_key_binding(KEY_CUT, "cut", put_time)
-mp.add_key_binding(KEY_TOGGLE_HARDSUBS, "toggle_hardsubs", toggle_hardsubs)
-mp.add_key_binding(KEY_TOGGLE_MP4, "toggle_mp4", toggle_mp4)
-mp.add_key_binding(KEY_TOGGLE_GENERATE_LIST, "toggle_generate_list", toggle_generate_list)
 mp.add_key_binding(KEY_TOGGLE_ACTION, "toggle_action", toggle_action)
 mp.add_key_binding(KEY_TOGGLE_USE_GLOBAL_DIR, "toggle_use_global_dir", toggle_use_global_dir)
