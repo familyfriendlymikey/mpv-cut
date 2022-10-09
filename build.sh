17fd6c27cd1687597aae9db313ac7c965a3d02ca
familyfriendlymikey <ozmichaelt@icloud.com>
2022-10-09 13:15:35 +0200

    improve build and release scripts/workflow

--- build.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p dist
-echo "#! /usr/bin/env node\n" > dist/make_cuts
-imbac -p make_cuts.imba >> dist/make_cuts
-cp main.lua dist/
--- release.sh
+++
@@ -1,1 +0,0 @@
-npx gh-pages --no-push --branch release --dist dist/ --message 1.0.1
